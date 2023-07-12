66371ac41e5ce60ebaba4513e3140f79e43261a6
furacas <s.furacas@outlook.com>
2023-07-12 21:58:03 +0800

    修改字符兼容wine

--- DllInjector/DllInjector.cpp
+++ DllInjector/DllInjector.cpp
@@ -2,12 +2,12 @@
 #include <TlHelp32.h> 
 #include <iostream>
 
-DWORD getProcId(const wchar_t* procName) {
+DWORD getProcId(const char* procName) {
     DWORD procId = 0;
     HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
     if (hSnap == INVALID_HANDLE_VALUE) {
-        std::wcout << L"Cannot create snapshot. Error: " << GetLastError() << L"\n";
+        std::cout << "Cannot create snapshot. Error: " << GetLastError() << "\n";
         return 0;
     }
 
@@ -16,75 +16,73 @@ DWORD getProcId(const wchar_t* procName) {
 
     if (Process32First(hSnap, &procEntry)) {
         do {
-            if (!_wcsicmp(procEntry.szExeFile, procName)) {
+            if (!_stricmp(procEntry.szExeFile, procName)) {
                 procId = procEntry.th32ProcessID;
                 break;
             }
         } while (Process32Next(hSnap, &procEntry));
 
         if (procId == 0) {
-            std::wcout << L"Cannot find process with given name.\n";
+            std::cout << "Cannot find process with given name.\n";
         }
     }
     else {
-        std::wcout << L"Cannot read first process. Error: " << GetLastError() << L"\n";
+        std::cout << "Cannot read first process. Error: " << GetLastError() << "\n";
     }
 
     CloseHandle(hSnap);
     return procId;
 }
 
-int wmain(int argc, wchar_t** argv) {
+int main(int argc, char** argv) {
     if (argc != 3) {
-        std::wcout << L"Usage: " << argv[0] << L" <dll_path> <process_name>\n";
+        std::cout << "Usage: " << argv[0] << " <dll_path> <process_name>\n";
         return 1;
     }
 
-    const wchar_t* dllPath = argv[1];
-    const wchar_t* procName = argv[2];
+    const char* dllPath = argv[1];
+    const char* procName = argv[2];
 
     DWORD procId = getProcId(procName);
     if (procId == 0) {
-        std::wcout << L"Cannot obtain process ID.\n";
+        std::cout << "Cannot obtain process ID.\n";
         system("PAUSE");
         return 1;
     }
 
-    std::wcout << L"Process ID is: " << procId << L"\n";
+    std::cout << "Process ID is: " << procId << "\n";
 
     HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
     if (handle == INVALID_HANDLE_VALUE) {
-        std::wcout << L"Cannot open process. Error: " << GetLastError() << L"\n";
+        std::cout << "Cannot open process. Error: " << GetLastError() << "\n";
         system("PAUSE");
         return 1;
     }
 
     void* loc = VirtualAllocEx(handle, 0, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     if (loc == NULL) {
-        std::wcout << L"Cannot allocate memory. Error: " << GetLastError() << L"\n";
+        std::cout << "Cannot allocate memory. Error: " << GetLastError() << "\n";
         system("PAUSE");
         CloseHandle(handle);
         return 1;
     }
 
-    if (!WriteProcessMemory(handle, loc, dllPath, (wcslen(dllPath) + 1) * sizeof(wchar_t), 0)) {
-        std::wcout << L"Cannot write to process memory. Error: " << GetLastError() << L"\n";
+    if (!WriteProcessMemory(handle, loc, dllPath, (strlen(dllPath) + 1) * sizeof(char), 0)) {
+        std::cout << "Cannot write to process memory. Error: " << GetLastError() << "\n";
         system("PAUSE");
         VirtualFreeEx(handle, loc, 0, MEM_RELEASE);
         CloseHandle(handle);
         return 1;
     }
 
-#ifdef _WIN64
-    //64位系统中的代码
-    FARPROC hLoadLibrary = GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryW");
-#else
-    //32位系统中的代码
-    FARPROC hLoadLibrary = GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA");
-#endif
+    #ifdef _WIN64
+        FARPROC hLoadLibrary = GetProcAddress(GetModuleHandle("Kernel32.dll"), "LoadLibraryW");
+    #else
+        FARPROC hLoadLibrary = GetProcAddress(GetModuleHandle("Kernel32.dll"), "LoadLibraryA");
+    #endif
 
     if (!hLoadLibrary) {
-        std::wcout << L"Cannot get LoadLibrary address. Error: " << GetLastError() << L"\n";
+        std::cout << "Cannot get LoadLibrary address. Error: " << GetLastError() << "\n";
         system("PAUSE");
         VirtualFreeEx(handle, loc, 0, MEM_RELEASE);
         CloseHandle(handle);
@@ -93,13 +91,13 @@ int wmain(int argc, wchar_t** argv) {
 
     HANDLE hThread = CreateRemoteThread(handle, 0, 0, (LPTHREAD_START_ROUTINE)hLoadLibrary, loc, 0, 0);
     if (hThread == NULL) {
-        std::wcout << L"Cannot create remote thread. Error: " << GetLastError() << L"\n";
+        std::cout << "Cannot create remote thread. Error: " << GetLastError() << "\n";
         system("PAUSE");
         VirtualFreeEx(handle, loc, 0, MEM_RELEASE);
         CloseHandle(handle);
         return 1;
     }
 
-    std::wcout << L"DLL injected successfully.\n";
+    std::cout << "DLL injected successfully.\n";
     return 0;
 }
